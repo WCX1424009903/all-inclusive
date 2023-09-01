38005599623feb4b940f9cc0eea491b9f301775b
wcx <wang1424009903@163.com>
2023-09-01 12:11:51 +0800

    增加sa-token工程

---
+++ sa-token/src/main/java/org/example/sa/token/SaTokenApplication.java
@@ -0,0 +1,18 @@
+package org.example.sa.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* sa-token服务
+* @author wcx
+* @date 2023/8/31
+*/
+@SpringBootApplication
+public class SaTokenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaTokenApplication.class,args);
+    }
+
+}
