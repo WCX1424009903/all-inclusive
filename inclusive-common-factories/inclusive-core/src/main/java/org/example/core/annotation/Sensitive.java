c116e7cea344fc129ac974ac508732206f65b234
wcx <wang1424009903@163.com>
2024-02-05 19:13:24 +0800

    脱敏注解完善

--- inclusive-common-factories/inclusive-core/src/main/java/org/example/core/annotation/Sensitive.java
+++ inclusive-common-factories/inclusive-core/src/main/java/org/example/core/annotation/Sensitive.java
@@ -1,6 +1,14 @@
 package org.example.core.annotation;
 
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.example.core.enums.SensitiveTypeEnum;
+import org.example.core.serializer.SensitiveSerializer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * 脱敏注解
@@ -8,6 +16,10 @@ import org.example.core.enums.SensitiveTypeEnum;
  * @author wcx
  * @date 2024/1/21
  */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@JacksonAnnotationsInside
+@JsonSerialize(using = SensitiveSerializer.class)
 public @interface Sensitive {
     /**
      * 脱敏数据类型
